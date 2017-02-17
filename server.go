b0b4a973ecae32eea4fab7721ead64c9ece02818
Andreas Koch <andy@ak7.io>
2017-02-17 22:48:12 +0100

    Respond with proper Alexa JSON
    see: https://developer.amazon.com/public/solutions/alexa/alexa-skills-kit/docs/alexa-skills-kit-interface-reference#response-format

--- server.go
+++ server.go
@@ -11,13 +11,13 @@ import (
 func NewServer(listenAddress string, config Config) (Server, error) {
 	return Server{
 		listenAddress: listenAddress,
-		appID: config.AppID,
-		}, nil
+		appID:         config.AppID,
+	}, nil
 }
 
 type Server struct {
 	listenAddress string
-	appID string
+	appID         string
 }
 
 func (server *Server) Run() error {
@@ -34,9 +34,13 @@ func (server *Server) handleRequest(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprint(w, "%s", serviceRequestModel.Request.Intent.Slots.ApplicationName.Value)
+	w.Header().Set("Content-Type", "application/json; charset=utf-8");
+	response := createSpeechResponse(fmt.Sprintf("Deploying %s", serviceRequestModel.RequestBody.Intent.Slots.ApplicationName.Value))
+	if err := writeJSONResponse(w, response); err != nil {
+		http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+		return
+	}
 }
-
 
 func readServiceRequest(httpRequest *http.Request) (ServiceRequest, error) {
 	var serviceRequest ServiceRequest
@@ -52,6 +56,17 @@ func readServiceRequest(httpRequest *http.Request) (ServiceRequest, error) {
 	return serviceRequest, nil
 }
 
+func writeJSONResponse(w http.ResponseWriter, serviceResponse ServiceResponse) error {
+
+	jsonBody, err := json.Marshal(serviceResponse)
+	if err != nil {
+		return err
+	}
+
+	fmt.Fprintf(w, "%s", jsonBody)
+	return nil
+}
+
 type ServiceRequest struct {
 	Session struct {
 		SessionID string `json:"sessionId"`
@@ -65,20 +80,61 @@ type ServiceRequest struct {
 		} `json:"user"`
 		New bool `json:"new"`
 	} `json:"session"`
-	Request struct {
-		Type string `json:"type"`
+	RequestBody struct {
+		Type      string `json:"type"`
 		RequestID string `json:"requestId"`
-		Locale string `json:"locale"`
+		Locale    string `json:"locale"`
 		Timestamp time.Time `json:"timestamp"`
 		Intent struct {
 			Name string `json:"name"`
 			Slots struct {
 				ApplicationName struct {
-					Name string `json:"name"`
+					Name  string `json:"name"`
 					Value string `json:"value"`
 				} `json:"ApplicationName"`
 			} `json:"slots"`
 		} `json:"intent"`
 	} `json:"request"`
 	Version string `json:"version"`
+}
+
+func createSpeechResponse(text string) ServiceResponse {
+	response := ServiceResponse{}
+	response.Version = "1.0"
+
+	response.ResponseBody.Card.Type = "Simple"
+	response.ResponseBody.Card.Title = "Deploy"
+	response.ResponseBody.Card.Content = text
+
+	response.ResponseBody.OutputSpeech.Type = "PlainText"
+	response.ResponseBody.OutputSpeech.Text = text
+	return response
+}
+
+type ServiceResponse struct {
+	Version string `json:"version"`
+	SessionAttributes struct {
+	} `json:"sessionAttributes,omitempty"`
+	ResponseBody struct {
+		OutputSpeech struct {
+			Type string `json:"type"`
+			Text string `json:"text"`
+		} `json:"outputSpeech,omitempty"`
+		Card struct {
+			Type    string `json:"type"`
+			Title   string `json:"title"`
+			Content string `json:"content"`
+		} `json:"card,omitempty"`
+		Reprompt         *Reprompt `json:"reprompt,omitempty"`
+		ShouldEndSession bool `json:"shouldEndSession"`
+	} `json:"response"`
+}
+
+type Reprompt struct {
+	OutputSpeech *OutputSpeech `json:"outputSpeech"`
+}
+
+type OutputSpeech struct {
+	Type string `json:"type"`
+	Text string `json:"text"`
 }
