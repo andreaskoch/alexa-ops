1fa00daf8e87adbbc544155e05ecc53059033a8f
Andreas Koch <andy@ak7.io>
2017-02-18 23:30:06 +0100

    Add server logging
    Info logging for all incoming requests
    Error logging for bad input (400) or server errors (500)

--- server.go
+++ server.go
@@ -6,42 +6,78 @@ import (
 	"time"
 	"encoding/json"
 	"io/ioutil"
+	"log"
 )
 
 func NewServer(listenAddress string, config Config) (Server, error) {
 	return Server{
 		listenAddress: listenAddress,
-		appID:         config.AppID,
+		config:        config,
 	}, nil
 }
 
 type Server struct {
 	listenAddress string
-	appID         string
+	config        Config
 }
 
 func (server *Server) Run() error {
-
 	http.HandleFunc("/", server.handleRequest)
-
 	return http.ListenAndServe(server.listenAddress, nil)
 }
 
 func (server *Server) handleRequest(w http.ResponseWriter, r *http.Request) {
+	server.logRequest(r)
+
 	serviceRequestModel, err := readServiceRequest(r)
 	if err != nil {
+		server.logError("Failed to decode request: %s", err.Error())
 		http.Error(w, "400 Bad Request", http.StatusBadRequest)
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json; charset=utf-8");
+	if _, err := requestMatchesApplicationID(serviceRequestModel, server.config); err != nil {
+		server.logError("Application ID does not match: %s", err.Error())
+		http.Error(w, "400 Bad Request", http.StatusBadRequest)
+		return
+	}
+
 	response := createSpeechResponse(fmt.Sprintf("Deploying %s", serviceRequestModel.RequestBody.Intent.Slots.ApplicationName.Value))
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8");
 	if err := writeJSONResponse(w, response); err != nil {
+		server.logError("Failed to write JSON response: %s", err.Error())
 		http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 }
 
+func (server *Server) logRequest(request *http.Request) {
+	log.Printf("[Info] "+"%s %s %q %q", request.RemoteAddr, request.Method, request.URL.String(), request.UserAgent())
+}
+
+func (server *Server) logInfo(format string, p ...interface{}) {
+	log.Printf("[Info] "+format, p...)
+}
+
+func (server *Server) logError(format string, p ...interface{}) {
+	log.Printf("[Error] "+format, p...)
+}
+
+// requestMatchesApplicationID checks if the given ServiceRequest matches the application ID
+// in the supplied configuration. If the application IDs match true and no error is returned.
+// If the application IDs don't match false and an error is returned.
+func requestMatchesApplicationID(request ServiceRequest, config Config) (bool, error) {
+	isMatch := request.Session.Application.ApplicationID == config.AppID
+	if isMatch {
+		return true, nil
+	}
+
+	return false, fmt.Errorf("Given: %s, Required: %s", request.Session.Application.ApplicationID, config.AppID)
+}
+
+// readServiceRequest reads reads the ServiceRequest model from the given http request.
+// Returns an error if the ServiceRequest could not be read.
 func readServiceRequest(httpRequest *http.Request) (ServiceRequest, error) {
 	var serviceRequest ServiceRequest
 	body, readBodyError := ioutil.ReadAll(httpRequest.Body)
